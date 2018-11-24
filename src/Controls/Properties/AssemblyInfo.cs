955f79ea66013ce2b94c08db53a2194d51e4c35c
Nelson Garcia <ngarcia@ipac.caltech.edu>
2018-11-24 13:18:18 -0800

    Initialize Controls project
    * Add new C# project src/Controls/Controls.csproj, which will host a
      class library of common WinAPI controls for Maseya projects
    * Add GlobalSuppressions.cs and stylecop.json files, which are
      identical to the files in Helper project.
    * Update AssemblyInfo.cs to specify assembly information and to be
      CLS-Compliant.

---
+++ src/Controls/Properties/AssemblyInfo.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Public Domain">
+//     Copyright (c) 2018 Nelson Garcia. All rights reserved. Licensed
+//     under GNU Affero General Public License. See LICENSE in project
+//     root for full license information, or visit
+//     https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Controls")]
+[assembly: AssemblyDescription("Common WinAPI controls for Maseya projects")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Public Domain")]
+[assembly: AssemblyProduct("Controls")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Nelson Garcia")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not
+// visible to COM components. If you need to access a type in this
+// assembly from COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is
+// exposed to COM
+[assembly: Guid("d3c13b99-e4f8-438f-aae1-c2fd8897a90a")]
+
+// Version information for an assembly consists of the following four
+// values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and
+// Revision Numbers by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
