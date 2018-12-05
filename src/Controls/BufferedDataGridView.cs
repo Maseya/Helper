659502cdb78ca88561b01cf1bac4d326d9fd3359
Nelson Garcia <swr.ngarcia@gmail.com>
2018-12-05 01:38:18 -0800

    Add BufferedDataGridView class
    * Add src/Controls/BufferedDataGridView.cs. This class provides an
      implementation of `DataGridView` that is double-buffered and
      user-drawn.

---
+++ src/Controls/BufferedDataGridView.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="BufferedDataGridView.cs" company="Public Domain">
+//     Copyright (c) 2018 Nelson Garcia. All rights reserved. Licensed
+//     under GNU Affero General Public License. See LICENSE in project
+//     root for full license information, or visit
+//     https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+namespace Maseya.Controls
+{
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// Provides an implementation of <see cref="DataGridView"/> that is
+    /// double-buffered and user-drawn.
+    /// </summary>
+    /// <remarks>
+    /// Use <see cref="BufferedDataGridView"/> over <see cref="
+    /// DataGridView"/> in situations where you need to display images
+    /// in a grid, otherwise you can get flicker from drawing them.
+    /// </remarks>
+    public class BufferedDataGridView : DataGridView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="
+        /// BufferedDataGridView"/> class.
+        /// </summary>
+        public BufferedDataGridView()
+        {
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.DoubleBuffer, true);
+        }
+    }
+}
