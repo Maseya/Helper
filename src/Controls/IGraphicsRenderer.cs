ea97fe663118734ce6d6c53ae5fc68d918811805
Nelson Garcia <ngarcia@ipac.caltech.edu>
2019-06-10 14:49:05 -0700

    Add IGraphicsRender class
    This class implements `IComponent` and has a `Draw` function that,
    when implemented, draws its state to a `Graphics` instance.

---
+++ src/Controls/IGraphicsRenderer.cs
@@ -0,0 +1,13 @@
+﻿namespace Maseya.Controls
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+
+    public interface IGraphicsRenderer : IComponent
+    {
+        event EventHandler Redraw;
+
+        void Draw(Graphics graphics);
+    }
+}
