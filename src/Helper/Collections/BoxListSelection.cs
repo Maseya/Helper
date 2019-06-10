e0abaa05ba6a6aea8d3f81613c25fc5b9576256b
Nelson Garcia <ngarcia@ipac.caltech.edu>
2019-06-10 15:24:08 -0700

    Add Helper.Collections namespace
    This namespace contains classes for maintaining generic lists that
    support undo/redo and have more advanced list selection functions as
    well as slightly faster write functions for complex selections.

---
+++ src/Helper/Collections/BoxListSelection.cs
@@ -0,0 +1,103 @@
+﻿// <copyright file="BoxListSelection.cs" company="Public Domain">
+//     Copyright (c) 2019 Nelson Garcia. All rights reserved. Licensed under
+//     GNU Affero General Public License. See LICENSE in project root for full
+//     license information, or visit https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+namespace Maseya.Helper.Collections
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+
+    public class BoxListSelection : ListSelection
+    {
+        public BoxListSelection(int index, Size size, int gridWidth)
+        {
+            if (size.Width < 0 || size.Height < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            Size = size;
+            MinIndex = index;
+            Count = size.Width * size.Height;
+            GridWidth = gridWidth;
+        }
+
+        public Size Size
+        {
+            get;
+        }
+
+        public override int Count
+        {
+            get;
+        }
+
+        public override int MinIndex
+        {
+            get;
+        }
+
+        public override int MaxIndex
+        {
+            get
+            {
+                return MinIndex + Count - 1;
+            }
+        }
+
+        public int GridWidth
+        {
+            get;
+        }
+
+        public override int this[int index]
+        {
+            get
+            {
+                if (!ContainsIndex(index))
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index));
+                }
+
+                return GetIndex(GetPoint(index));
+            }
+        }
+
+        public override bool ContainsIndex(int index)
+        {
+            return new Rectangle(Point.Empty, Size).Contains(GetPoint(index));
+        }
+
+        public override ListSelection Move(int offset)
+        {
+            return new BoxListSelection(MinIndex + offset, Size, GridWidth);
+        }
+
+        public override IEnumerator<int> GetEnumerator()
+        {
+            for (var y = 0; y < Size.Height; y++)
+            {
+                var index = GetIndex(new Point(0, y));
+                for (var x = 0; x < Size.Width; x++)
+                {
+                    yield return index++;
+                }
+            }
+        }
+
+        private int GetIndex(Point point)
+        {
+            return MinIndex + point.X + (point.Y * GridWidth);
+        }
+
+        private Point GetPoint(int index)
+        {
+            return new Point(
+                (index - MinIndex) % GridWidth,
+                (index - MinIndex) / GridWidth);
+        }
+    }
+}
---
+++ src/Helper/IByteDataConverter.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="IByteDataConverter.cs" company="Public Domain">
+//     Copyright (c) 2019 Nelson Garcia. All rights reserved. Licensed under
+//     GNU Affero General Public License. See LICENSE in project root for full
+//     license information, or visit https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+namespace Maseya.Helper
+{
+    using System.Collections.Generic;
+
+    public interface IByteDataConverter<T>
+    {
+        int SizeOfItem
+        {
+            get;
+        }
+
+        T GetItem(IEnumerable<byte> bytes);
+
+        IEnumerable<byte> GetBytes(T item);
+    }
+}
