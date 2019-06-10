2c831cb2346c5a9ad992bc73fe97027cfa520ae3
Nelson Garcia <ngarcia@ipac.caltech.edu>
2019-06-10 14:52:12 -0700

    Add IImageRenderer interface
    This interface inherits from `IGraphicsRender` and implements a
    function `RenderImage` which returns an `Image`.

---
+++ src/Controls/IImageRenderer.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IImageRenderer.cs" company="Public Domain">
+//     Copyright (c) 2019 Nelson Garcia. All rights reserved. Licensed under
+//     GNU Affero General Public License. See LICENSE in project root for full
+//     license information, or visit https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+namespace Maseya.Controls
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+
+    public interface IImageRenderer : IGraphicsRenderer
+    {
+        Image RenderImage();
+    }
+}
