6e1ea27b1e92a53e0e808799c544aacf4a1409c7
Nelson Garcia <ngarcia@ipac.caltech.edu>
2019-06-10 14:28:02 -0700

    Add SizeEventArgs class
    This class is an `EventArgs` class that has a `Size` property to
    be used with any size-changing methods.

---
+++ src/Controls/SizeEventArgs.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="SizeEventArgs.cs" company="Public Domain">
+//     Copyright (c) 2019 Nelson Garcia. All rights reserved. Licensed under
+//     GNU Affero General Public License. See LICENSE in project root for full
+//     license information, or visit https://www.gnu.org/licenses/#AGPL
+// </copyright>
+
+namespace Maseya.Controls
+{
+    using System;
+    using System.Drawing;
+
+    public class SizeEventArgs : EventArgs
+    {
+        public SizeEventArgs(Size size)
+        {
+            Size = size;
+        }
+
+        public SizeEventArgs(int width, int height)
+            : this(new Size(width, height))
+        {
+        }
+
+        public Size Size
+        {
+            get;
+            set;
+        }
+    }
+}
